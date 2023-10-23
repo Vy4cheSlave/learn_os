ca950544052d4b42039cfb82f767f52cdbf60faf
Vy4cheSlave <slav.subocheff@yandex.ru>
2023-10-23 18:05:42 +0700

    figured out how 2 display characters on the screen

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,17 +1,44 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-use bootloader_api::{entry_point, BootInfo};
-use core::panic::PanicInfo;
+mod vga;
 
-entry_point!(kernel_main);
+#[allow(unused_imports)] //
+use bootloader_api::{config, info};
+#[allow(unused_imports)] //
+use core::fmt::Write;
+#[allow(unused_imports)] //
+use vga::FrameBufferWriter;
 
-fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+#[allow(dead_code)] //
+const CONFIG: bootloader_api::BootloaderConfig = {
+    let mut config = bootloader_api::BootloaderConfig::new_default();
+    config.kernel_stack_size = 100 * 1024; // 100 KiB
+    config
+};
+bootloader_api::entry_point!(kernel_main);
+
+#[allow(unused_variables)] //
+fn kernel_main(boot_info: &'static mut info::BootInfo) -> ! {
+    // vga object
+    let mut framebuffer_writer = match &mut boot_info.framebuffer {
+        info::Optional::Some(frame_buffer) => {
+            let framebuffer_info = frame_buffer.info().clone();
+            FrameBufferWriter::new(frame_buffer.buffer_mut(), framebuffer_info)
+        }
+        info::Optional::None => panic!("Failed to get framebuffer from boot info."),
+    };
+
+    // write example on vga
+    framebuffer_writer
+        .write_str("-Staralis, Klim Sanich \n-Da \n-Pyatikratno pervarenniy KAL \n-DA")
+        .expect("FrameBufferWriter 'write_str()' error");
+
     loop {}
 }
 
 // This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
